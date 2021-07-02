27c22801830eab1e22f9da796d190b7e86421c3d
Tim McMaster <tim@mcmaster.id.au>
2021-07-02 17:25:27 +1000

    version 0.0.2

--- lib/widget_tester.dart
+++ lib/widget_tester.dart
@@ -2,6 +2,3 @@ library widget_tester;
 
 export 'src/widget_tester.dart';
 export 'src/widget_tester_options.dart';
-// export 'src/widget_tester_providers.dart';
-// export 'src/widget_tester_size_controls.dart';
-// export 'src/widget_tester_view_pane.dart';
